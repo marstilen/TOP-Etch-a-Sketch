8ff53196e3cbc91c1db05c40879eabe049d09546
marstilen <tilen.mars@proton.me>
2025-11-17 08:19:38 +0100

    Create a gird

--- scripts/main.js
+++ scripts/main.js
@@ -7,11 +7,16 @@ let gridContainer = document.querySelector("#gridContainer");
 console.log(gridContainer);
 let gridSize = 64;
 for (let i = 0; i < gridSize; i++) {
-    let card = document.createElement("div");
-    card.setAttribute("class", "card")
-    card.setAttribute("id", `card${i}`)
-    gridContainer.appendChild(card);
-    card.addEventListener('mouseenter', function(e) {
-        changeColor(e.target);
-    });
+    let row = document.createElement("div");
+    row.setAttribute("class", "row");
+    gridContainer.appendChild(row);
+    for (let i = 0; i < 64; i++) {   
+        let card = document.createElement("div");
+        card.setAttribute("class", "card")
+        card.setAttribute("id", `card${i}`)
+        row.appendChild(card);
+        card.addEventListener('mouseenter', function(e) {
+            changeColor(e.target);
+        });
+    }
 }
